a0861013f0e2884f3ad0d7604594ec719b62987a
SilentBobIsCoding <necr@tut.by>
2013-02-27 19:09:44 +0300

    Engine is updated to score the results

--- assets/www/script/core.js
+++ assets/www/script/core.js
@@ -17,24 +17,210 @@ var MyApp = {
 
 
     start: function(){
+        this.x = 3;
+        this.y = 3;
+
+
         this.init();
 
         this.viewPort = $('#viewport');
-        this.board = new MyApp.controls.Board(3, 3);
+        this.board = new MyApp.controls.Board(this.x , this.y);
         this.board.renderTo('viewport');
-
         MyApp.utils.addListener(this.board, 'cellclick', this._cellClickHandler, this);
+
+        this.label = new MyApp.controls.Label();
+        this.label.renderTo('viewport');
+        this.label.dom.css('display', 'block');
+
+        this.startButton = new MyApp.controls.Button({
+            text: 'Start new game'
+        });
+        this.startButton.renderTo('viewport');
+        MyApp.utils.addListener(this.startButton, 'click', this.gameStart, this);
+
+
 
     },
 
-    _lastOperation: false,
+    _data: null,
+    _lastOperation: 'O',
+
+    gameStart: function(){
+        this.resetBoard();
+        this._lastOperation = 'O';     // starting with "X"
+
+        var op = this._lastOperation === 'O' ? 'O' : 'X';
+        this.label.setText(op + ' - Make your move');
+    },
+
+    resetBoard: function(){
+        this._data = [];
+        this.label.setText('');
+        this.board.clearCells();
+    },
 
     _cellClickHandler: function(o){
 
-        var sign = this._lastOperation ? 0 : 1;
-        this._lastOperation = sign;
-        var text = sign ? 'x' : 'o';
-        this.board.setCellContent(o.col, o.row, text);
+        if (this._moveAvailable(o.row, o.col)){
+
+            var sign = this._lastOperation === 'X' ? 'O' : 'X';
+            this._updateData(o.row, o.col, sign);
+
+            this._lastOperation = sign;
+            var text = sign === 'X' ? 'x' : 'o';
+            this.board.setCellContent(o.row, o.col, text);
+
+            var res = this._checkLineReady();
+            if (res){
+                alert(res.sign + ' is the winner.');
+            }
+
+        } else {
+            this.board.showCellDirty(o.row, o.col);
+        }
+
+    },
+
+    _moveAvailable: function(row, col){
+        return this._data[row]
+                    ? this._data[row][col] === undefined
+                    : true;
+    },
+
+    _updateData: function(row, col, sign){
+        if (!this._data[row]){
+            this._data[row] = [];
+        }
+
+        this._data[row][col] = sign;
+    },
+
+    _checkLineReady: function(){
+        var res = {};
+
+        // check horizontals
+        for (var i = 0, l = this._data.length; i < l; i++){
+            var line = this._data[i];
+
+            if (!line){
+                continue;
+            }
+
+            var s = {
+                x: 0,
+                o: 0
+            };
+            for (var j = 0, k = line.length; j < k; j++){
+                var v = line[j];
+                if (v === 'X'){
+                    s.x++;
+                } else if (v === 'O'){
+                    s.o++;
+                }
+            }
+
+            if (s.x === this.x || s.o === this.x){
+                res.finished = true;
+                res.line = i;
+                res.sign = line[0];
+                break;
+            }
+        }
+
+        if (res.finished){
+            return res;
+        }
+
+        // check verticals
+        for (var i = 0; i < this.y; i++){
+            var s = {
+                x: 0,
+                o: 0
+            };
+
+            for (var j = 0; j < this.x; j++){
+                var line = this._data[j];
+                if (!line){
+                    continue;
+                }
+
+                var v = line[i];
+                if (v === 'X'){
+                    s.x++;
+                } else if (v === 'O'){
+                    s.o++;
+                }
+            }
+
+            if (s.x === this.y || s.o === this.y){
+                res.finished = true;
+                res.col = i;
+                res.sign = this._data[0][i];
+                break;
+            }
+        }
+
+        if (res.finished){
+            return res;
+        }
+
+        // check first diagonal
+        var s = {
+            x: 0,
+            o: 0
+        };
+
+        for (var i = 0; i < this.x; i++){
+            if (this._data[i] && this._data[i][i]){
+                var v = this._data[i][i];
+
+                if (v === 'X'){
+                    s.x++;
+                } else if (v === 'O'){
+                    s.o++;
+                }
+            }
+        }
+
+        if (s.x === this.y || s.o === this.y){
+            res.finished = true;
+            res.col = i;
+            res.sign = this._data[0][0];
+        }
+
+        if (res.finished){
+            return res;
+        }
+
+        // check second diagonal
+        var s = {
+            x: 0,
+            o: 0
+        }
+
+        for (var i = 0; i < this.x; i++){
+            if (this._data[i] && this._data[i][this.x - 1 - i]){
+                var v = this._data[i][this.x - 1 - i];
+
+                if (v === 'X'){
+                    s.x++;
+                } else if (v === 'O'){
+                    s.o++;
+                }
+            }
+        }
+
+        if (s.x === this.y || s.o === this.y){
+            res.finished = true;
+            res.col = i;
+            res.sign = this._data[0][this.x - 1];
+        }
+
+        if (res.finished){
+            return res;
+        }
+
+        return null;
     }
 };
 
@@ -54,7 +240,6 @@ MyApp.controls = {
                         '<tr>' +
                             '{{repeat ' + y + ' "<td>z</td>"}}' +
                         '</tr>' +
-
                     '{{/repeat}}' +
                 '</table>';
 
@@ -89,7 +274,7 @@ MyApp.controls = {
             initEvents(this._table);
         };
 
-        this.setCellContent = function(col, row, text){
+        this.setCellContent = function(row, col, text){
             console.log(text);
             if (!this._table){
                 return;
@@ -98,6 +283,66 @@ MyApp.controls = {
             var rowEl = $(this._table.find('TR')[row]);
             var cellEl = $(rowEl.children()[col]);
             cellEl.html(text);
+        };
+
+        this.clearCells = function(){
+            this._table.find('TD').html('&nbsp');
+        };
+
+        this.showCellDirty = function(row, col){
+
+        };
+
+    },
+
+    Label: function(){
+        // TODO remove hardcode
+        var id = 'label';
+
+        var getHtml = function(){
+            // TODO
+            return '<span id="' + id + '" style="textlabel">Dummy</span>';
+        };
+
+        this.renderTo = function(parentId){
+            $('#' + parentId).append(getHtml());
+            this.dom = $('#' + id);
+        };
+
+        this.setText = function(text){
+            if (this.dom){
+                this.dom.html(text);
+            }
+        };
+    },
+
+    Button: function(cfg){
+        cfg = cfg || {};
+
+
+        var id = cfg.id || 'button';
+
+        var getHtml = function(){
+            return '<button id="' + id + '">' + cfg.text + '</button>';
+        };
+
+        var that = this;
+
+        var initEvents = function(button){
+
+            button.on('click', function(e){
+                e.stopPropagation();
+                MyApp.utils.fireEvent(that, 'click');
+            });
+
+        };
+
+        this.renderTo = function(parentId){
+            $('#' + parentId).append(getHtml());
+
+            this.dom = $('#' + id);
+            initEvents(this.dom);
+
         };
 
     }
@@ -119,7 +364,7 @@ MyApp.utils = {
         }
 
         if (!eventObj){
-            console.log('Sender is null');
+            console.log('Sender is null or no listeners');
             return;
         }
 
